9ef27c02890087fde18bb02b000de4c235ede8e0
hillyleopard133 <brokfairy@gmail.com>
2025-11-15 02:26:27 +0000

    More sprites

---
+++ UnityProject/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Upgrades", menuName = "Scriptable Objects/Upgrades")]
+public class Upgrades : ScriptableObject
+{
+    
+}
+
+
+/*
+ *Upgrades
+ *
+ * quality - improve smoothie value
+ * tree quantity - improve collection rates
+ * fertiliser - improve apples per tree?
+ * harvester - automate collecting
+ *
+ */
